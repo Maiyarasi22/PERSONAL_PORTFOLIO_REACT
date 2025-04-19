d34336e438fcada84d31f117bfe50ec30e11347f
Maiyarasi22 <maiyarasi22@gmail.com>
2025-04-19 22:25:31 +0530

    Added some animations in home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,38 @@
 import { Link } from 'react-router-dom';
-import LogoTitle from '../../assets/images/logo-s.png';
+// import LogoTitle from '../../assets/images/logo-m.webp';
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react';
 
 const Home = () =>{
+
+    const [letterClass, setLetterClass]  = useState('text-animate')
+    const nameArray = ['M','a','i','y','a','r','a','s','i']
+
+    useEffect(() =>{
+        const timer = setTimeout(()=>{
+            setLetterClass('text-animate-hover')
+        },4000)
+        return () =>clearTimeout(timer)
+    },[])
 
     return(
         <div className="container home-page">
             <div className="text-zone">
-                <h1> Hi,<br/> I'm
-                <img src={LogoTitle} alt="developer" />
-                lobodan<br/>
-                web developer
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i</span>
+                    <span className={`${letterClass} _13`}>,</span>
+                    <br/>
+                    <span className={`${letterClass} _14`}>I</span>
+                    <span className={`${letterClass} _15`}>'</span>
+                    <span className={`${letterClass} _16`}>m</span>
+                {/* <img src={LogoTitle} alt="developer" /> */}
+                <AnimatedLetters letterClass = {letterClass} 
+                strArray={nameArray} 
+                idx={17}/>
                 </h1>
-                <h2>Frontend Developer</h2>
+                <h2>Aspiring Frontend Developer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,6 @@
 import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/logo-m.webp'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faHome,faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -10,7 +9,7 @@ const Sidebar = ()=> (
 <div className='nav-bar'>
     <Link className='logo' to='/'>
         <img src={LogoS} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        
 
     </Link>
 <nav>
